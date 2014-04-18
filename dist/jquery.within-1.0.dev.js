5500596c51c94657547b6be1f9db39a337d41248
linlymatsumura <linly.matsumura@gmail.com>
2014-04-18 21:22:21 +0900

    new

---
+++ dist/jquery.within-1.0.dev.js
@@ -0,0 +1,62 @@
+// Copyright (c) 2014 Linly Matsumura.
+
+/**
+ * @fileoverview 画像を内接／外接リサイズして正方形に収めるjQueryプラグイン
+ * @version 1.0
+ * @requires jQuery
+ * @license Code licensed under MIT
+ * @author LNLY(@linlymatsumura)
+ */
+
+(function($) {
+    $.fn.within = function(ops){
+        var defaults = {width : 100, type : 'contain', bgColor : '#333'}, pref;
+
+        if(typeof arguments[0] === 'object'){
+            pref = $.extend(defaults, ops);
+        }
+        else{
+            pref = {
+                width : arguments[0] || defaults.width,
+                type : arguments[1] || defaults.type,
+                bgColor : arguments[2] || defaults.bgColor
+            };
+        }
+
+        $(this).each(function(){
+            var $this = $(this), src = $this.attr('src');
+            $this
+                .after('<span></span>')
+                .next()
+                .attr({
+                    'class' : $this.attr('class'),
+                    'id' : $this.attr('id')
+                })
+                .css({
+                    'display' : 'inline-block',
+                    'width' : pref.width + 'px',
+                    'height' : (pref.height || pref.width) + 'px',
+                    'background-color' : pref.bgColor,
+                    'background-position' : 'center center',
+                    'background-repeat' : 'no-repeat',
+                    'background-size' : pref.type,
+                    'background-image' : 'url(' + src + ')',
+                    'vertical-align' : 'middle'
+                });
+
+            $this.remove();
+        });
+
+        return $(this);
+    };
+
+    $('[data-within]').each(function(){
+        var ops = $(this).attr('data-within').split(',');
+        
+        if (ops[0] === '') {
+            ops = undefined;
+        }
+
+        $(this).within.apply(this,ops);
+    });
+})(jQuery);
