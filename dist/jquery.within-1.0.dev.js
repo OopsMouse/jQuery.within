ac821fcdbe87684cb0efd626819cbdb9b5881127
Noda Shimpei <noda.sin@gmail.com>
2015-09-25 20:12:02 +0900

    Automatically resize for responsive designe when window is resized.

--- dist/jquery.within-1.0.dev.js
+++ dist/jquery.within-1.0.dev.js
@@ -1,62 +1,39 @@
-// Copyright (c) 2014 Linly Matsumura.
-
-/**
- * @fileoverview 画像を内接／外接リサイズして正方形に収めるjQueryプラグイン
- * @version 1.0
- * @requires jQuery
- * @license Code licensed under MIT:https://github.com/linlymatsumura/jQuery.within/blob/master/LICENSE.md
- * @author LNLY(@linlymatsumura)
- */
-
 (function($) {
-    $.fn.within = function(ops){
-        var defaults = {width : 100, type : 'contain', bgColor : '#333'}, pref;
+    var resizeRectImg = function() {
+        $('.data-img-rectangle').remove();
 
-        if(typeof arguments[0] === 'object'){
-            pref = $.extend(defaults, ops);
-        }
-        else{
-            pref = {
-                width : arguments[0] || defaults.width,
-                type : arguments[1] || defaults.type,
-                bgColor : arguments[2] || defaults.bgColor
-            };
-        }
+        $('.img-rectangle').each(function() {
 
-        $(this).each(function(){
-            var $this = $(this), src = $this.attr('src');
+            var $this = $(this),
+                src   = $this.attr('src')
+                width = $this.width();
+
             $this
-                .after('<span></span>')
-                .next()
-                .attr({
-                    'class' : $this.attr('class'),
-                    'id' : $this.attr('id')
-                })
-                .css({
-                    'display' : 'inline-block',
-                    'width' : pref.width + 'px',
-                    'height' : (pref.height || pref.width) + 'px',
-                    'background-color' : pref.bgColor,
-                    'background-position' : 'center center',
-                    'background-repeat' : 'no-repeat',
-                    'background-size' : pref.type,
-                    'background-image' : 'url(' + src + ')',
-                    'vertical-align' : 'middle'
-                });
+            .after('<span></span>')
+            .next()
+            .attr({
+                'class' : $this.attr('class') + ' ' + 'data-img-rectangle',
+                'id' : $this.attr('id')
+            })
+            .css({
+                'display' : 'inline-block',
+                'width' : width + 'px',
+                'height' : width + 'px',
+                'background-position' : 'center center',
+                'background-repeat' : 'no-repeat',
+                'background-size' : 'cover',
+                'background-image' : 'url(' + src + ')',
+                'vertical-align' : 'middle'
+            });
 
-            $this.remove();
+            $this.css('display', 'none');
         });
+    }
 
-        return $(this);
-    };
+    resizeRectImg();
 
-    $('[data-within]').each(function(){
-        var ops = $(this).attr('data-within').split(',');
-        
-        if (ops[0] === '') {
-            ops = undefined;
-        }
-
-        $(this).within.apply(this,ops);
+    $(window).resize(function() {
+        resizeRectImg();
     });
+
 })(jQuery);
